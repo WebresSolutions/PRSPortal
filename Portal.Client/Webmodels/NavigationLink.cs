04198dc19903f95eb7713d003ea85ab5aff0f149
ws-jsamson <jordan@ws1.com.au>
2025-10-20 11:25:08 +1100

    Check in

---
+++ Portal.Client/Webmodels/NavigationLink.cs
@@ -0,0 +1,22 @@
+﻿namespace Portal.Client.Webmodels;
+
+public class NavigationLink()
+{
+
+    /// <summary>
+    /// The display name of the link.
+    /// </summary>
+    public string Link { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The icons
+    /// </summary>
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// Navigation links for sub menus.
+    /// </summary>
+    public NavigationLink[] SubLinks { get; set; } = [];
+}
